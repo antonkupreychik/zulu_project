95c864f7fcc11572c762f90c3ee28dac0c7bf7e0
akupreychik <kupreychikanton@gmail.com>
2023-09-11 22:13:36 +0300

    add some code at main

--- src/CreditCard.java
+++ src/CreditCard.java
@@ -1,4 +1,13 @@
 class CreditCard {
+
+    public CreditCard(long number, int year, int month, String paymentSystem, Person holder) {
+        this.number = number;
+        this.year = year;
+        this.month = month;
+        this.paymentSystem = paymentSystem;
+        this.holder = holder;
+    }
+
     long number;
     int year;
     int month;
