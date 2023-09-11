6a586112ae08ea84d9ed2ce6ea0290556307e973
akupreychik <kupreychikanton@gmail.com>
2023-09-11 22:13:52 +0300

    add cooments

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,22 @@ public class Main {
         CreditCard creditCard = new CreditCard(1l, 2022, 3, null,null);
         CardValidator cardValidator = new CardValidator();
         System.out.println(cardValidator.validate(creditCard));
+
+
+         /*    public CreditCard(long number, int year, int month, String paymentSystem, Person holder) {
+        this.number = number;
+        this.year = year;
+        this.month = month;
+        this.paymentSystem = paymentSystem;
+        this.holder = holder;
+    }
+
+    public CreditCard(long number, int year, int month, Person holder) {
+        this.number = number;
+        this.year = year;
+        this.month = month;
+        this.holder = holder;
+        this.paymentSystem = Main.PaymentSystemValidator.verifyPaymentSystem(number);
+    }*/
     }
 }
